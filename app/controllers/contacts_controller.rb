3d44d3f3cf598cd9abd5f4b155271e9f60b946bd
Willem-Jan Stienstra <wjstienstra@gmail.com>
2015-11-16 13:47:43 +0000

    Contact form validation

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,9 +6,11 @@ class ContactsController < ApplicationController
     def create
         @contact = Contact.new(contact_params)
         if @contact.save
-            redirect_to new_contact_path, notice: "Message sent."
+            flash[:success] = 'Message sent.'
+            redirect_to new_contact_path
         else
-            redirect_to new_contact_path, notice: "Error occured."
+            flash[:danger] = 'Error occured, message has not been sent.'
+            redirect_to new_contact_path
         end
     end
   

3095650c753b34547cfc2d01579b6af87c5f94bf
Willem-Jan Stienstra <wjstienstra@gmail.com>
2015-11-19 09:19:38 +0000

    install devise

---
+++ app/models/rail.rb
@@ -0,0 +1,6 @@
+class Rail < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end

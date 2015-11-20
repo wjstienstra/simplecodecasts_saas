4bda043be055680e50593f0a5af023785463e539
Willem-Jan Stienstra <wjstienstra@gmail.com>
2015-11-20 13:55:27 +0000

    updated files for stripe

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,15 +1,14 @@
-class Users::RegistrationsController < DeviseRegistrationsController
-
-    def create
-        super do |resource|
-            if params[:plan]
-            resource.plan.id = params[:plan]
-                if resource.plan.id == 2
-                    resource.save_with_payment
-                else
-                    resource.save
-                end
-            end
+class Users::RegistrationsController < Devise::RegistrationsController
+  def create
+    super do |resource|
+      if params[:plan]
+        resource.plan_id = params[:plan]
+        if resource.plan_id == 2
+          resource.save_with_payment
+        else
+          resource.save
         end
+      end
     end
+  end
 end

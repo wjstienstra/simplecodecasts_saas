12cfd68a1399d812c3d7f601d92a2e27ddc55797
Willem-Jan Stienstra <wjstienstra@gmail.com>
2015-11-20 10:33:24 +0000

    added devise controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,14 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   belongs_to :plan
+  attr_accessor :strip_card_token
+  
+  def save_with_payment
+    if valid?
+      customer = Stripe::Customer.create(description: email, plan: plan_id, card: strip_card_token)
+      self.stripe_customer_token = customer.id
+      save!
+    end
+  end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151119121519) do
+ActiveRecord::Schema.define(version: 20151120102433) do
 
   create_table "contacts", force: true do |t|
     t.string   "name"
@@ -20,7 +20,6 @@ ActiveRecord::Schema.define(version: 20151119121519) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -35,6 +34,8 @@ ActiveRecord::Schema.define(version: 20151119121519) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "stripe_customer_token"
+    t.integer  "plan_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true

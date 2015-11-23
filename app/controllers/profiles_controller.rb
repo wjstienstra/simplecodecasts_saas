5e5f341a51b7c8617ced61e818d64f3a4318b3fc
Willem-Jan Stienstra <wjstienstra@gmail.com>
2015-11-23 17:00:47 +0000

    home improvements

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,7 +2,7 @@ class ProfilesController < ApplicationController
   def new
     # form where a user can fill out their own profile.
     @user = User.find( params[:user_id] )
-    @profile = @user.Profile.new
+    @profile = Profile.new
   end
   def create 
     @user = User.find( params[:user_id] )

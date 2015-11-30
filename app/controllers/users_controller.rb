fccea06602f9ea72396e8ca68f68c96b67d92816
Willem-Jan Stienstra <wjstienstra@gmail.com>
2015-11-30 09:10:39 +0000

    impoved user index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
     before_action :authenticate_user!
     
     def index
-        
+       @user = User.includes(:profile)
     end
     
     def show
